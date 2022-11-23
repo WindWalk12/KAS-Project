805d5e8b4b217553fb16b2f298d2c7052bda5797
Mikkel <mikkelhess@icloud.com>
2022-11-23 13:09:53 +0100

    tilføjet strings til tal

--- src/gui/App.java
+++ src/gui/App.java
@@ -7,10 +7,10 @@ public class App {
     public static void main(String[] args) {
 
         Controller.initStorage();
-        System.out.println(Controller.getTilmeldninger().get(0).udregnSamletPris());
-        System.out.println(Controller.getTilmeldninger().get(1).udregnSamletPris());
-        System.out.println(Controller.getTilmeldninger().get(2).udregnSamletPris());
-        System.out.println(Controller.getTilmeldninger().get(3).udregnSamletPris());
+        System.out.println("Samlet pris for Deltager 1: " + Controller.getTilmeldninger().get(0).udregnSamletPris());
+        System.out.println("Samlet pris for Deltager 2: " + Controller.getTilmeldninger().get(1).udregnSamletPris());
+        System.out.println("Samlet pris for Deltager 3: " + Controller.getTilmeldninger().get(2).udregnSamletPris());
+        System.out.println("Samlet pris for Deltager 4: " + Controller.getTilmeldninger().get(3).udregnSamletPris());
     }
 
 }
