08fbd8a96c9e5499b20340c91601260ec3267ff4
WindWalk <nhygta@hotmail.com>
2022-11-23 09:45:59 +0100

    Opdateret link-attribute

--- src/Application/Model/Tilmelding.java
+++ src/Application/Model/Tilmelding.java
@@ -1,21 +1,17 @@
 package Application.Model;
 
-import java.util.ArrayList;
-
 public class Tilmelding {
     private double samletPris;
     private Deltager deltager;
     private Konference konference;
     private Hotel hotel;
-    private ArrayList<Udflugt> udflugter;
     private Ledsager ledsager;
 
-    public Tilmelding(double samletPris, Deltager deltager, Konference konference, Hotel hotel, ArrayList<Udflugt> udflugter, Ledsager ledsager) {
+    public Tilmelding(double samletPris, Deltager deltager, Konference konference, Hotel hotel, Ledsager ledsager) {
         this.samletPris = samletPris;
         this.deltager = deltager;
         this.konference = konference;
         this.hotel = hotel;
-        this.udflugter = udflugter;
         this.ledsager = ledsager;
     }
 }
