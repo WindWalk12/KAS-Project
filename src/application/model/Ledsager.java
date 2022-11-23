02bfbb04befd40cf5dbecadee1956acc61a3dad8
JohnLe <johnle@hotmail.dk>
2022-11-23 11:35:02 +0100

    Rettelser

--- src/application/model/Ledsager.java
+++ src/application/model/Ledsager.java
@@ -9,7 +9,8 @@ public class Ledsager extends Person {
     public Ledsager(String navn, String adresse) {
         super(navn, adresse);
     }
-
+//---------------------------------------------------------------------------------------------------------------------
+// association --> 0..* Udflugter
     public ArrayList<Udflugt> getUdflugter() {
         return new ArrayList<>(udflugter);
     }
@@ -26,5 +27,5 @@ public class Ledsager extends Person {
             udflugt.removeLedsager(this);
         }
     }
-
+    //--------------------------------------------------------------------------------------------------------
 }
