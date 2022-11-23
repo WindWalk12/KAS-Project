cd04402df987189bd039ef22de2e6913f861ec3c
Mikkel <mikkelhess@icloud.com>
2022-11-23 09:49:09 +0100

    jeg har lavet udflugt klassen

--- src/Application/Model/Udflugt.java
+++ src/Application/Model/Udflugt.java
@@ -1,4 +1,18 @@
 package Application.Model;
 
+import java.time.LocalDate;
+
 public class Udflugt {
+
+    private LocalDate dato;
+    private double pris;
+    private String navn;
+    private Konference konference;
+
+    public Udflugt(LocalDate dato, double pris, String navn, Konference konference) {
+        this.dato = dato;
+        this.pris = pris;
+        this.navn = navn;
+        this.konference = konference;
+    }
 }
