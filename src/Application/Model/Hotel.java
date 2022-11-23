94646a0585323e1ab4d4ac99caef25eee7264ded
JohnLe <johnle@hotmail.dk>
2022-11-23 09:42:55 +0100

    Jeg har modelleret Hotel klassen

--- src/Application/Model/Hotel.java
+++ src/Application/Model/Hotel.java
@@ -3,15 +3,17 @@ package Application.Model;
 import java.util.ArrayList;
 
 public class Hotel {
-    private int vaerelsesNr;
+
     private double pris;
+    private double prisDobbeltVaerelse;
     private String navn;
     private String adresse;
     private ArrayList<Tilmelding> tilmeldinger = new ArrayList<>();
     private ArrayList<Service> services = new ArrayList<>();
 
-    public Hotel(int vaerelsesNr, String navn, String adresse) {
-        this.vaerelsesNr = vaerelsesNr;
+    public Hotel(double pris, double prisDobbeltVaerelse, String navn, String adresse) {
+        this.prisDobbeltVaerelse = prisDobbeltVaerelse;
+        this.pris = pris;
         this.navn = navn;
         this.adresse = adresse;
     }
