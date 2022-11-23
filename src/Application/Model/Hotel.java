ea9146a09dc242d8b19fa8b5ec02ed2e0d17d725
JohnLe <johnle@hotmail.dk>
2022-11-23 10:11:32 +0100

    Jeg har modelleret Serivce klassen

--- src/Application/Model/Hotel.java
+++ src/Application/Model/Hotel.java
@@ -10,6 +10,8 @@ public class Hotel {
     private String adresse;
     private ArrayList<Tilmelding> tilmeldinger = new ArrayList<>();
     private ArrayList<Service> services = new ArrayList<>();
+    private ArrayList<Konference> konferencer = new ArrayList<>();
+
 
     public Hotel(double pris, double prisDobbeltVaerelse, String navn, String adresse) {
         this.prisDobbeltVaerelse = prisDobbeltVaerelse;
@@ -25,15 +27,36 @@ public class Hotel {
     public ArrayList<Service> getServices() {
         return new ArrayList<>(services);
     }
-    public Service createService (String navn, double pris) {
+
+    public Service createService(String navn, double pris) {
         Service service = new Service(navn, pris);
         services.add(service);
         return service;
     }
+
     public void removeService(Service service) {
         if (services.contains(service)) {
             services.remove(service);
         }
     }
     //---------------------------------------------------------------------------------------------------------------------
+    // association --> 0..* Tilmeldinger
+
+    public ArrayList<Tilmelding> getTilmeldinger() {
+        return new ArrayList<>(tilmeldinger);
+    }
+
+    public void addTilmelding(Tilmelding tilmelding) {
+        if (!tilmeldinger.contains(tilmelding)) {
+            tilmeldinger.add(tilmelding);
+            tilmelding.setHotel(this);
+        }
+    }
+
+    public void removeTilmelding(Tilmelding tilmelding) {
+        if (tilmeldinger.contains(tilmelding)) {
+            tilmeldinger.remove(tilmelding);
+            tilmelding.setHotel(null);
+        }
+    }
 }
