356bff91f61cc84d9b37c80989255668603210cb
JohnLe <johnle@hotmail.dk>
2022-11-23 09:46:40 +0100

    Jeg har modelleret Serivce klassen

--- src/Application/Model/Service.java
+++ src/Application/Model/Service.java
@@ -2,4 +2,11 @@ package Application.Model;
 
 public class Service {
 
+    private String navn;
+    private double pris;
+
+    public Service (String navn, double pris){
+        this.navn = navn;
+        this.pris = pris;
+    }
 }
