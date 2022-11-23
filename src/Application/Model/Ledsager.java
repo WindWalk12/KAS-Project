0d2dfe327472c4e735329706dafa252f19086446
Mikkel <mikkelhess@icloud.com>
2022-11-23 09:12:22 +0100

    Jeg har opdateret Ledsager

--- src/Application/Model/Ledsager.java
+++ src/Application/Model/Ledsager.java
@@ -1,4 +1,16 @@
 package Application.Model;
 
-public class Ledsager {
+import java.util.ArrayList;
+
+public class Ledsager extends Person {
+
+    private ArrayList<Udflugt> udflugter = new ArrayList<>();
+    private Tilmelding tilmelding;
+
+    public Ledsager(String navn, int alder, String adresse, String tlfNummer, String email) {
+        super(navn, alder, adresse, tlfNummer, email);
+
+
+
+    }
 }
