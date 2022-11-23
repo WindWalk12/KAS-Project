68fb4a7d0be5b95fa89018a3600bf0eb3f3542ec
Mikkel <mikkelhess@icloud.com>
2022-11-23 09:06:39 +0100

    Jeg har opdateret Person + deltager klassen

--- src/Application/Model/Deltager.java
+++ src/Application/Model/Deltager.java
@@ -1,4 +1,17 @@
 package Application.Model;
 
+import java.util.ArrayList;
+
 public class Deltager extends Person {
+
+    private boolean foredragsholder;
+    private boolean privatPerson;
+    private String firma;
+
+
+
+    public Deltager(String navn, int alder, String adresse, String tlfNummer, String email) {
+        super(navn, alder, adresse, tlfNummer, email);
+
+    }
 }
--- src/Application/Model/Person.java
+++ src/Application/Model/Person.java
@@ -8,4 +8,11 @@ private String adresse;
 private String tlfNummer;
 private String email;
 
+    public Person(String navn, int alder, String adresse, String tlfNummer, String email) {
+        this.navn = navn;
+        this.alder = alder;
+        this.adresse = adresse;
+        this.tlfNummer = tlfNummer;
+        this.email = email;
+    }
 }
